2d1a4bd748dd1ea53a2debb326c25b56bd71e7f3
Abigail Howell <aahowel3@asu.edu>
2021-06-10 10:12:59 -0700

    scripts for checking sources of contamination from unaligned reads in MIC and MAC FACS samples

---
+++ check_contamination/blast_check/mic_contamination/blast_unmapped.sh
@@ -0,0 +1,3 @@
+/work/aahowel3/bam2fastq/bam2fastq --no-aligned -o unaligned#.fastq ../../../bam/Mic_S1_L001_tomic_sorted_rmdup.bam
+spades.py -k 21,33,55,77,99,127 --careful -o spades_assembly -1 unaligned_1.fastq -2 unaligned_2.fastq
+blastn -remote -db nr -query spades_assembly/contigs.fasta -evalue 1e-06 -show_gis -num_alignments 10 -num_descriptions 10 -out contigs.fasta.blastn
