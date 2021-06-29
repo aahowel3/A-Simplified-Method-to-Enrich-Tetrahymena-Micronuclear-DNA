2a20889d8b0b8ea946da4e475cb541712b2d3d5f
Abigail Howell <aahowel3@asu.edu>
2021-06-29 09:16:35 -0700

    Delete analyze_coverage.sh

--- coverage/mac_coverage/analyze_coverage.sh
+++
@@ -1,8 +0,0 @@
-#USAGE: command line input should look like $bash analyze coverage.sh *Mac*
-
-#use input argument @ for unspecified number of arguments
-for arg in "$@"
-do
-        file=$(basename "$arg" _Mac_tomic_coverage.txt)
-	Rscript analyze_coverage.R "${file}_IESs_inmic.tsv" "${file}_Mac_tomic_coverage.txt"
-done 
