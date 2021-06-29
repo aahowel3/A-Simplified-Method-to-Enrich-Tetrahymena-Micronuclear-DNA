f33a1988bda4b87e100e6c861c1987501bf73220
Abigail Howell <aahowel3@asu.edu>
2021-06-28 19:15:16 -0700

    Update flowsort_curation_wc_2.sh

--- fishers_exact/wholecell_subset/flowsort_curation_wc_2.sh
+++ fishers_exact/wholecell_subset/flowsort_curation_wc_2.sh
@@ -1,4 +1,4 @@
-#USAGE: command line input should look like $bash flowsort_curation_2_wc.sh /work/aahowel3/flowsortdata/2931489_Howell/wholecell_subset/*R1.trim.fq
+#USAGE: command line input should look like $bash flowsort_curation_2_wc.sh *R1.trim.fq
 
 #creates directories to store intermediate files 
 mkdir -p sam2
@@ -9,7 +9,7 @@ do
 #strips extension from input file 
 	file=$(basename "$filename" _R1.trim.fq)
 #aligns paired end fastqs to reference 
-	bwa mem /work/aahowel3/flowsortdata/2931489_Howell/fishers_rerun/mic_mac_combinedreference_rDNA.fasta /work/aahowel3/flowsortdata/2931489_Howell/wholecell_subset/"${file}"_R1.trim.fq /work/aahowel3/flowsortdata/2931489_Howell/wholecell_subset/"${file}"_R2.trim.fq > sam2/"${file}"_tomicmac.sam
+	bwa mem mic_mac_combinedreference_rDNA.fasta "${file}"_R1.trim.fq "${file}"_R2.trim.fq > sam2/"${file}"_tomicmac.sam
 #converts sam to bam, sorts and indexes bam
 	samtools view -S -b sam2/"${file}"_tomicmac.sam > bam2/"${file}"_tomicmac.bam
 	samtools sort -o bam2/"${file}"_tomicmac_sorted.bam bam2/"${file}"_tomicmac.bam
