5450c6aff6cb9f9802361843471afe6f718b49df
Abigail Howell <aahowel3@asu.edu>
2021-06-28 18:59:18 -0700

    Update blast_unmapped.sh

--- check_contamination/blast_check/mac_contamination/blast_unmapped.sh
+++ check_contamination/blast_check/mac_contamination/blast_unmapped.sh
@@ -1,3 +1,6 @@
-/work/aahowel3/bam2fastq/bam2fastq --no-aligned -o unaligned#.fastq ../../../bam/Mac_S2_L001_tomac_sorted_rmdup.bam 
+#convert unaligned reads in MAC_to_macreference to fastqs
+bam2fastq --no-aligned -o unaligned#.fastq Mac_S2_L001_tomac_sorted_rmdup.bam 
+#assemble into contigs
 spades.py -k 21,33,55,77,99,127 --careful -o spades_assembly -1 unaligned_1.fastq -2 unaligned_2.fastq
+#blast assembled contigs 
 blastn -remote -db nr -query spades_assembly/contigs.fasta -evalue 1e-06 -show_gis -num_alignments 10 -num_descriptions 10 -out contigs.fasta.blastn
