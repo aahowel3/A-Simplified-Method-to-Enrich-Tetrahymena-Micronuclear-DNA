dc3225a38b902323f7aca062a6e1e2c09172dff0
Abigail Howell <aahowel3@asu.edu>
2021-06-28 20:41:37 -0700

    Update and rename flowsort_curation_2_wc.sh to flowsort_curation_2_wc_04e.sh

--- simulations/simulations_1x/flowsort_curation_2_wc_04e.sh
+++ simulations/simulations_1x/flowsort_curation_2_wc_04e.sh
@@ -4,10 +4,8 @@
 mkdir -p sam
 mkdir -p bam
 
-#just type commands verbatim its just one set of reads not iterative
-
 #aligns paired end fastqs to reference 
-bwa mem /work/aahowel3/flowsortdata/2931489_Howell/fishers_rerun/mic_mac_combinedreference_rDNA.fasta wc_simulated1.fq wc_simulated2.fq > sam/wc_simulated_tomicmac.sam
+bwa mem /path/to/combined_reference wc_simulated1.fq wc_simulated2.fq > sam/wc_simulated_tomicmac.sam
 #converts sam to bam, sorts and indexes bam
 samtools view -S -b sam/wc_simulated_tomicmac.sam > bam/wc_simulated_tomicmac.bam
 samtools sort -o bam/wc_simulated_tomicmac_sorted.bam bam/wc_simulated_tomicmac.bam
