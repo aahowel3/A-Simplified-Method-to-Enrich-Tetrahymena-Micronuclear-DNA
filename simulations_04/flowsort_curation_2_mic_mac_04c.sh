2530dddd821faa46cbb8043d344e8a45cf00be0c
Abigail Howell <aahowel3@asu.edu>
2021-06-28 20:46:59 -0700

    reorder file names

---
+++ simulations_04/flowsort_curation_2_mic_mac_04c.sh
@@ -0,0 +1,34 @@
+#USAGE: command line input should look like $bash flowsort_curation_2_wc.sh 
+
+#creates directories to store intermediate files 
+mkdir -p sam_mic
+mkdir -p bam_mic
+
+#just type commands verbatim its just one set of reads not iterative
+
+#aligns paired end fastqs to reference 
+bwa mem /path/to/combined_refernce mic_simulated1.fq mic_simulated2.fq > sam_mic/mic_simulated_tomicmac.sam
+#converts sam to bam, sorts and indexes bam
+samtools view -S -b sam_mic/mic_simulated_tomicmac.sam > bam_mic/mic_simulated_tomicmac.bam
+samtools sort -o bam_mic/mic_simulated_tomicmac_sorted.bam bam_mic/mic_simulated_tomicmac.bam
+samtools rmdup bam_mic/mic_simulated_tomicmac_sorted.bam bam_mic/mic_simulated_tomicmac_sorted_rmdup.bam
+samtools index bam_mic/mic_simulated_tomicmac_sorted_rmdup.bam 
+samtools view -h -b -F 4 bam_mic/mic_simulated_tomicmac_sorted_rmdup.bam > bam_mic/mic_simulated_tomicmac_sorted_rmdup_mapped.bam
+samtools index bam_mic/mic_simulated_tomicmac_sorted_rmdup_mapped.bam
+
+
+#creates directories to store intermediate files
+mkdir -p sam_mac   
+mkdir -p bam_mac   
+
+#aligns paired end fastqs to reference
+bwa mem /path/to/combined_refernce mac_simulated1.fq mac_simulated2.fq > sam_mac/mac_simulated_tomicmac.sam
+#converts sam to bam, sorts and indexes bam
+samtools view -S -b sam_mac/mac_simulated_tomicmac.sam > bam_mac/mac_simulated_tomicmac.bam
+samtools sort -o bam_mac/mac_simulated_tomicmac_sorted.bam bam_mac/mac_simulated_tomicmac.bam
+samtools rmdup bam_mac/mac_simulated_tomicmac_sorted.bam bam_mac/mac_simulated_tomicmac_sorted_rmdup.bam
+samtools index bam_mac/mac_simulated_tomicmac_sorted_rmdup.bam
+samtools view -h -b -F 4 bam_mac/mac_simulated_tomicmac_sorted_rmdup.bam > bam_mac/mac_simulated_tomicmac_sorted_rmdup_mapped.bam
+samtools index bam_mac/mac_simulated_tomicmac_sorted_rmdup_mapped.bam
+
+
