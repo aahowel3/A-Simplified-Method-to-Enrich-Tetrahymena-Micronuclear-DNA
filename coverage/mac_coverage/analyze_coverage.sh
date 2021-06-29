1e86cce87961c49afc3a7721757d0a33a01287d7
Abigail Howell <aahowel3@asu.edu>
2021-06-28 19:09:40 -0700

    Update analyze_coverage.sh

--- coverage/mac_coverage/analyze_coverage.sh
+++ coverage/mac_coverage/analyze_coverage.sh
@@ -4,6 +4,5 @@
 for arg in "$@"
 do
         file=$(basename "$arg" _Mac_tomic_coverage.txt)
-#	echo /work/aahowel3/flowsortdata/2931489_Howell/retention_scores/"${file}_IESs_inmic.csv" "${file}_Mac_tomic_coverage.txt"
-	Rscript /work/aahowel3/flowsortdata/2931489_Howell/retention_scores/coverage/analyze_coverage.R /work/aahowel3/flowsortdata/2931489_Howell/retention_scores/"${file}_IESs_inmic.tsv" "${file}_Mac_tomic_coverage.txt"
+	Rscript analyze_coverage.R "${file}_IESs_inmic.tsv" "${file}_Mac_tomic_coverage.txt"
 done 
