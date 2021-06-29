8c52e537a82c9c60b87a8d069af0fb85d546ba90
Abigail Howell <aahowel3@asu.edu>
2021-06-28 20:31:15 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,8 @@ bash ./wholecell_data_02/subset_02a.sh /path/to/downloaded/wholecell_data
 bash ./fishers_exact_03/fishers_rerun_2_03a.sh ./data_01/*R1_001.trim.fastq.gz
 
 bash ./fishers_exact_03/wholecell_subset/flowsort_curation_wc_2_03b.sh ./data_01/*R1_001.trim.fastq.gz
+samtools view -b -h -F 4 file.bam > mapped.bam
+samtools view FACSsample_toconcatref_mapped.bam | grep -v "XA:" | grep -v "SA:" | awk '$3 ~ /scf/' | wc -l 
+samtools view FACSsample_toconcatref_mapped.bam | grep -v "XA:" | grep -v "SA:" | awk '$3 ~ /chr/' | wc -l 
 
 R ./fishers_exact_03/fishers_rerun_ftests_03c.R 
