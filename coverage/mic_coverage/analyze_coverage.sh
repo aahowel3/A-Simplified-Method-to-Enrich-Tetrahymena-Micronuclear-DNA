cac9c32515621dba7130ac516254f69a80a06506
Abigail Howell <aahowel3@asu.edu>
2021-06-29 09:17:30 -0700

    Delete analyze_coverage.sh

--- coverage/mic_coverage/analyze_coverage.sh
+++
@@ -1,8 +0,0 @@
-#USAGE: command line input should look like $bash analyze coverage.sh *Mic*
-
-#use input argument @ for unspecified number of arguments
-for arg in "$@"
-do
-        file=$(basename "$arg" _Mic_tomic_coverage.txt)
-	Rscript analyze_coverage.R "${file}_IESs_inmic.tsv" "${file}_Mic_tomic_coverage.txt"
-done 
