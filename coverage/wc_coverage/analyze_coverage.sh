f1a21fc8fea7137c326bf511f37ad8c2647fbd1d
Abigail Howell <aahowel3@asu.edu>
2021-06-29 09:17:43 -0700

    Delete analyze_coverage.sh

--- coverage/wc_coverage/analyze_coverage.sh
+++
@@ -1,8 +0,0 @@
-#USAGE: command line input should look like $bash analyze coverage.sh *SB210*
-
-#use input argument @ for unspecified number of arguments
-for arg in "$@"
-do
-        file=$(basename "$arg" _SB210_tomic_coverage.txt)
-	Rscript analyze_coverage.R "${file}_IESs_inmic.tsv" "${file}_SB210_tomic_coverage.txt"
-done 
