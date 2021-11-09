a813d95770aa88c3e8d61f66991483f02561883c
Abigail Howell <aahowel3@asu.edu>
2021-11-09 10:50:48 -0800

    Add files via upload

--- simulations_04/simulations_1x/flowsort_curation_2_wc_04e.sh
+++ simulations_04/simulations_1x/flowsort_curation_2_wc_04e.sh
@@ -1,14 +1,15 @@
 #USAGE: command line input should look like $bash flowsort_curation_2_wc.sh 
 
 #creates directories to store intermediate files 
-mkdir -p sam
-mkdir -p bam
+mkdir -p ./simulations_04/simulations_1x/sam
+mkdir -p ./simulations_04/simulations_1x/bam
 
 #aligns paired end fastqs to reference 
-bwa mem /path/to/combined_reference wc_simulated1.fq wc_simulated2.fq > sam/wc_simulated_tomicmac.sam
+bwa mem ./data_01/mic_mac_combinedreference_rDNA.fasta ./simulations_04/simulations_1x/wc_simulated1.fq ./simulations_04/simulations_1x/wc_simulated2.fq > ./simulations_04/simulations_1x/sam/wc_simulated_tomicmac.sam
 #converts sam to bam, sorts and indexes bam
-samtools view -S -b sam/wc_simulated_tomicmac.sam > bam/wc_simulated_tomicmac.bam
-samtools sort -o bam/wc_simulated_tomicmac_sorted.bam bam/wc_simulated_tomicmac.bam
-samtools rmdup bam/wc_simulated_tomicmac_sorted.bam bam/wc_simulated_tomicmac_sorted_rmdup.bam
-samtools index bam/wc_simulated_tomicmac_sorted_rmdup.bam 
+samtools view -S -b ./simulations_04/simulations_1x/sam/wc_simulated_tomicmac.sam > ./simulations_04/simulations_1x/bam/wc_simulated_tomicmac.bam
+samtools sort -o ./simulations_04/simulations_1x/bam/wc_simulated_tomicmac_sorted.bam ./simulations_04/simulations_1x/bam/wc_simulated_tomicmac.bam
+samtools rmdup ./simulations_04/simulations_1x/bam/wc_simulated_tomicmac_sorted.bam ./simulations_04/simulations_1x/bam/wc_simulated_tomicmac_sorted_rmdup.bam
+samtools view -b -h -F 4 ./simulations_04/simulations_1x/bam/wc_simulated_tomicmac_sorted_rmdup.bam > ./simulations_04/simulations_1x/bam/wc_simulated_tomicmac_sorted_rmdup_mapped.bam
+samtools index ./simulations_04/simulations_1x/bam/wc_simulated_tomicmac_sorted_rmdup_mapped.bam 
 
