fb9e7af8e26d614d0ddfce99a0d8a1e5e3620168
Abigail Howell <aahowel3@asu.edu>
2021-11-09 10:48:11 -0800

    Add files via upload

--- fishers_exact_03/wholecell_subset/flowsort_curation_wc_2_03b.sh
+++ fishers_exact_03/wholecell_subset/flowsort_curation_wc_2_03b.sh
@@ -1,19 +1,19 @@
 #USAGE: command line input should look like $bash flowsort_curation_2_wc.sh *R1.trim.fq
 
 #creates directories to store intermediate files 
-mkdir -p sam2
-mkdir -p bam2
+mkdir -p ./fishers_exact_03/wholecell_subset/sam2
+mkdir -p ./fishers_exact_03/wholecell_subset/bam2
 
 for filename in $@
 do
 #strips extension from input file 
-	file=$(basename "$filename" _R1.trim.fq)
+	file=$(basename "$filename" _R1.fq)
 #aligns paired end fastqs to reference 
-	bwa mem mic_mac_combinedreference_rDNA.fasta "${file}"_R1.trim.fq "${file}"_R2.trim.fq > sam2/"${file}"_tomicmac.sam
+	bwa mem ./data_01/mic_mac_combinedreference_rDNA.fasta ./wholecell_data_02/"${file}"_R1.fq ./wholecell_data_02/"${file}"_R2.fq > ./fishers_exact_03/wholecell_subset/sam2/"${file}"_tomicmac.sam
 #converts sam to bam, sorts and indexes bam
-	samtools view -S -b sam2/"${file}"_tomicmac.sam > bam2/"${file}"_tomicmac.bam
-	samtools sort -o bam2/"${file}"_tomicmac_sorted.bam bam2/"${file}"_tomicmac.bam
-	samtools rmdup bam2/"${file}"_tomicmac_sorted.bam bam2/"${file}"_tomicmac_sorted_rmdup.bam
-	samtools index bam2/"${file}"_tomicmac_sorted_rmdup.bam 
-
+	samtools view -S -b ./fishers_exact_03/wholecell_subset/sam2/"${file}"_tomicmac.sam > ./fishers_exact_03/wholecell_subset/bam2/"${file}"_tomicmac.bam
+	samtools sort -o ./fishers_exact_03/wholecell_subset/bam2/"${file}"_tomicmac_sorted.bam ./fishers_exact_03/wholecell_subset/bam2/"${file}"_tomicmac.bam
+	samtools rmdup ./fishers_exact_03/wholecell_subset/bam2/"${file}"_tomicmac_sorted.bam ./fishers_exact_03/wholecell_subset/bam2/"${file}"_tomicmac_sorted_rmdup.bam
+	samtools view -b -h -F 4 ./fishers_exact_03/wholecell_subset/bam2/"${file}"_tomicmac_sorted_rmdup.bam > ./fishers_exact_03/wholecell_subset/bam2/"${file}"_tomicmac_sorted_rmdup_mapped.bam
+	samtools index ./fishers_exact_03/wholecell_subset/bam2/"${file}"_tomicmac_sorted_rmdup_mapped.bam 
 done
