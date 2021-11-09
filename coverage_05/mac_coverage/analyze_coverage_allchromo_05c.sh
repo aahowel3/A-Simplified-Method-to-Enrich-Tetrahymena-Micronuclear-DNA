f8f7f88d00c5647ef6c10eb9cd294773a4739396
Abigail Howell <aahowel3@asu.edu>
2021-11-09 10:56:08 -0800

    Add files via upload

--- coverage_05/mac_coverage/analyze_coverage_allchromo_05c.sh
+++ coverage_05/mac_coverage/analyze_coverage_allchromo_05c.sh
@@ -4,5 +4,7 @@
 for arg in "$@"
 do
         file=$(basename "$arg" _Mac_tomic_coverage.txt)
-	Rscript analyze_coverage_allchromo.R "${file}_IESs_inmic.tsv" "${file}_Mac_tomic_coverage.txt" >> wholechromo.mac.txt
+#	chromo=$(echo $file| grep -oP '.*?(?=\.)')
+#	chromo_number=$(echo "${chromo: -1}")
+	Rscript ./coverage_05/analyze_coverage_allchromo_05d.R ./coverage_05/"${file}"_IESs_inmic.tsv ./coverage_05/mac_coverage/"${file}_Mac_tomic_coverage.txt" >> wholechromo.mac.txt
 done 
